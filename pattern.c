f189ca1466b59152e392e523fe9058d7d6b33875
IshitaAsthana <44469738+IshitaAsthana@users.noreply.github.com>
2018-11-05 22:44:33 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -1,1 +1,24 @@
-pascal's triangle
+#include<stdio.h>
+int main()
+{
+  int l=1,k=1,n,i;
+  printf("\nEnter number of rows: ");
+  scanf("%d",&n);
+  for(i=1;i<n;i++)
+  {
+    k=2*i-1;
+    for(l=n;l>i;l--)
+      printf(" ");
+    printf("*");
+    for(l=1;l<(2*(i-1));l++)
+      printf(" ");
+    if(i>1)
+      printf("*");
+    printf("\n");
+  }
+  k=2*i-1;
+  for(l=1;l<=k;l++)
+    printf("*");
+  return 0;
+}
+    
